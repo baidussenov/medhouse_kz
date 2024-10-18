893326f23b128ee5c811f1587309a9bc2aebbb04
Alikhan Baidussenov <a.baydusenov@mail.ru>
2024-10-18 08:26:15 +0000

    Removed encryption

--- backend/hasher.js
+++
@@ -1,7 +0,0 @@
-const argon2 = require('argon2');
-
-(async () => {
-    const password = '9IyrqiytuBxNCNX'; // Replace this with your actual admin password
-    const hash = await argon2.hash(password);
-    console.log('Hashed password:', hash);
-})();
--- backend/middlewares/adminAuth.js
+++ backend/middlewares/adminAuth.js
@@ -1,4 +1,3 @@
-const argon2 = require('argon2')
 const dotenv = require('dotenv')
 
 // Load .env variables
@@ -15,10 +14,7 @@ const authenticateAdmin = async (req, res, next) => {
         // Compare the hash of the provided password with the stored hash in .env
         const adminTokenHash = process.env.ADMIN_ACCESS_TOKEN
 
-        // Use argon2 to verify the password
-        const isValid = await argon2.verify(adminTokenHash, password)
-
-        if (!isValid) {
+        if (adminTokenHash !== password) {
             return res.status(403).json({ message: 'Invalid admin password' })
         }
 
